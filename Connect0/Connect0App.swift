02e93f79f7ac7fcf3337839b3aaf089f8822439d
Rick <RIck@de-rijk.com>
2024-10-01 20:32:35 +0200

    Initial Commit

---
+++ Connect0/Connect0App.swift
@@ -0,0 +1,17 @@
+//
+//  Connect0App.swift
+//  Connect0
+//
+//  Created by Rick de Rijk on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Connect0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Connect0Tests/Connect0Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Connect0Tests.swift
+//  Connect0Tests
+//
+//  Created by Rick de Rijk on 01/10/2024.
+//
+
+import Testing
+@testable import Connect0
+
+struct Connect0Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
